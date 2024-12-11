31b0d37e83d8a513c6bba1aeed42606d31a5f0ca
Lord Lumineer <56554075+LordLumineer@users.noreply.github.com>
2024-12-11 15:21:35 -0500

    load tests

---
+++ load-test/first-test.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  thresholds: {
+    // Assert that 99% of requests finish within 3000ms.
+    http_req_duration: ["p(99) < 3000"],
+  },
+  // Ramp the number of virtual users up and down
+  stages: [
+    { duration: '30s', target: 20 },
+    { duration: '1m30s', target: 10 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('http://localhost/api/user/eef968e2-7989-4000-af26-2e874cae8e1c/image');
+  // Validate response status
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
